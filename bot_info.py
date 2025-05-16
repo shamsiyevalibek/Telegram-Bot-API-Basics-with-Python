f3280ba9c8cf2852eecc500b4944e8bd578b4e26
unknown <shamsiyevalibek90@gmail.com>
2025-05-16 16:16:08 +0300

    jonibekkks

--- bot_info.py
+++ bot_info.py
@@ -9,4 +9,12 @@ def get_bot_info():
     - Return bot's name and username
     - Show verification status
     """
-    pass
+    url=f"{BASE_URL}/getMe"
+    response=requests.get(url)
+    data=response.json()
+    user=data["result"]
+    a=user["first_name"]
+    s=user["username"]
+    return a,s
+print(get_bot_info())
+
