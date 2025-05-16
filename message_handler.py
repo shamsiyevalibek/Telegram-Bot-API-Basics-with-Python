a9e98485df28d21473cb8bfb9ba8211f261ad2ff
unknown <shamsiyevalibek90@gmail.com>
2025-05-16 17:05:14 +0300

    jonibekchang

--- message_handler.py
+++ message_handler.py
@@ -1,7 +1,23 @@
-def handle_message(message):
-    """
-    Optional Challenge: Conditional Responses
-    - Handle different message inputs
-    - Return appropriate responses
-    """
-    pass
+import requests
+from pprint import pprint
+from config import TOKEN, BASE_URL
+
+import requests
+
+def reply_to_last_user():
+    response = requests.get(f"{BASE_URL}/getUpdates")
+    data=response.json()
+    updates=data['result'][-1]
+    chat_id = updates['message']['chat']['id']
+    massege = updates['message']['text'].lower()
+
+    if 'hi' in massege:
+        reply_text = 'Hello!'
+    elif 'by' in massege:
+        reply_text = 'Goodbye!'
+    else:
+        reply_text ='xato'
+    send_url = BASE_URL + "/sendMessage"
+    requests.post(send_url, params={'chat_id': chat_id,'text':reply_text })
+    pprint(send_url)
+reply_to_last_user()
