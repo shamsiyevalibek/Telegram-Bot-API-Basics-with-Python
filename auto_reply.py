d74c20e1913c785a173355bbf2836bc404bb0509
unknown <shamsiyevalibek90@gmail.com>
2025-05-16 16:02:52 +0300

    salir64

--- auto_reply.py
+++ auto_reply.py
@@ -9,4 +9,15 @@ def reply_to_last_user():
     - Extract chat_id
     - Send response message
     """
-    pass
+    url=f"{BASE_URL}/getUpdates"
+    send=f"{BASE_URL}/sendMessage"
+    response=requests.get(url)
+    data=response.json()
+    updates=data['result'][-1]
+    chat_id=updates['message']['chat']['id']
+    requests.post(send,params={
+        'chat_id': chat_id,
+        'text': "alibrk"
+    })
+    return chat_id
+print(reply_to_last_user())
